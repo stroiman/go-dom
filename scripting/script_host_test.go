f0e852c0e950b0bc9449b516410bbc8006230b7d
Peter Strøiman <peter@stroiman.com>
2024-11-14 18:42:04 +0100

    Remove unnecessary Ordered marker for tests

--- scripting/script_host_test.go
+++ scripting/script_host_test.go
@@ -3,10 +3,9 @@ package scripting_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	// . "github.com/stroiman/go-dom/scripting"
 )
 
-var _ = Describe("ScriptHost", Ordered, func() {
+var _ = Describe("ScriptHost", func() {
 	ctx := InitializeContext()
 
 	Describe("Script host", func() {
