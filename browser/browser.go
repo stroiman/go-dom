85417386022572b18d9cb07109ae18d81801f501
Peter Strøiman <peter@stroiman.com>
2024-11-17 06:49:49 +0100

    Extract the TestRoundTripper to new file

--- browser/browser.go
+++ browser/browser.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 	"net/http/cookiejar"
-	"net/http/httptest"
 	"net/url"
 )
 
@@ -54,23 +53,6 @@ func (b *Browser) Open(url string) Document {
 		panic("err")
 	}
 	return ParseHtmlStream(resp.Body)
-}
-
-// A TestRoundTripper is an implementation of the [http.RoundTripper] interface
-// that communicates directly with an [http.Handler] instance.
-type TestRoundTripper struct{ http.Handler }
-
-func (h TestRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	// if req.Host != "" {
-	// 	// TODO: Not tested, nowhere near the case where we can test this yet, but
-	// 	// the idea is if we are serving content directly from an http.Handler, any
-	// 	// external script or CSS reference (when implemented) should be downloaded,
-	// 	// so the test still works if you reference content from CDN.
-	// 	return http.DefaultTransport.RoundTrip(req)
-	// }
-	rec := httptest.NewRecorder()
-	h.ServeHTTP(rec, req)
-	return rec.Result(), nil
 }
 
 func NewBrowserFromHandler(handler http.Handler) *Browser {
