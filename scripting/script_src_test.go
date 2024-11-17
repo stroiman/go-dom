9cd3df9cbc9d446392ee93d613dbc504f7f9663f
Peter Strøiman <peter@stroiman.com>
2024-11-17 11:04:09 +0100

    Skeleton server test

---
+++ scripting/script_src_test.go
@@ -0,0 +1,12 @@
+package scripting_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	// . "github.com/stroiman/go-dom/scripting"
+)
+
+var _ = Describe("Script downloading", func() {
+	ctx := InitializeContextWithEmptyHtml()
+
+})
