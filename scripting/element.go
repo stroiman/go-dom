f0270cf1789831b95604578bd6c9a5799ac2093d
Peter Strøiman <peter@stroiman.com>
2024-11-23 08:02:54 +0100

    Simplify JS Element wrapper with helper function

--- scripting/element.go
+++ scripting/element.go
@@ -8,24 +8,16 @@ import (
 
 func CreateElement(host *ScriptHost) *v8.FunctionTemplate {
 	iso := host.iso
-	res := v8.NewFunctionTemplateWithError(
-		iso,
-		func(args *v8.FunctionCallbackInfo) (*v8.Value, error) {
-			return nil, v8.NewTypeError(iso, "Illegal Constructor")
-		},
-	)
-	proto := res.PrototypeTemplate()
-	proto.SetAccessorPropertyCallback("outerHTML",
-		func(info *v8.FunctionCallbackInfo) (*v8.Value, error) {
-			ctx := host.MustGetContext(info.Context())
-			this, ok := ctx.GetCachedNode(info.This())
-			if e, e_ok := this.(Element); e_ok && ok {
-				return v8.NewValue(iso, e.OuterHTML())
-			} else {
-				return nil, v8.NewTypeError(iso, "Not an instance of Element")
-			}
-		}, nil, v8.ReadOnly)
-	instanceTemplate := res.GetInstanceTemplate()
-	instanceTemplate.SetInternalFieldCount(1)
-	return res
+	builder := NewIllegalConstructorBuilder[Element](host)
+	builder.instanceLookup = func(ctx *ScriptContext, this *v8.Object) (Element, error) {
+		element, ok := ctx.GetCachedNode(this)
+		if e, e_ok := element.(Element); e_ok && ok {
+			return e, nil
+		} else {
+			return nil, v8.NewTypeError(iso, "Not an instance of Element")
+		}
+	}
+	helper := builder.NewPrototypeBuilder()
+	helper.CreateReadonlyProp("outerHTML", Element.OuterHTML)
+	return builder.constructor
 }
