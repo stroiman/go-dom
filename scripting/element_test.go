6f4d1f8ad1defe5f22a59b6a26384a682de2cb55
Peter Strøiman <peter@stroiman.com>
2024-11-25 09:17:48 +0100

    Remove unnecessary test var

--- scripting/element_test.go
+++ scripting/element_test.go
@@ -7,28 +7,27 @@ import (
 )
 
 var _ = Describe("V8 Element", func() {
-	ctx := InitializeContextWithEmptyHtml()
-
 	It("It should be a direct descendant of Node", func() {
+		ctx := NewTestContext()
 		Expect(
 			ctx.RunTestScript("Object.getPrototypeOf(Element.prototype) === Node.prototype"),
 		).To(BeTrue())
 	})
 
 	It("Should have nodeType == 1", func() {
-		ctx.Window().LoadHTML(`<div id="1" class="foo"></div>`)
+		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
 		Expect(ctx.RunTestScript("document.body.nodeType")).To(BeEquivalentTo(1))
 	})
 
 	It("Should support getAtribute", func() {
-		ctx.Window().LoadHTML(`<div id="1" class="foo"></div>`)
+		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
 		Expect(ctx.RunTestScript(
 			`document.getElementById("1").getAttribute("class")`,
 		)).To(Equal("foo"))
 	})
 
 	It("Should support insertAdjacentHTML", func() {
-		ctx.Window().LoadHTML(`<div id="1" class="foo"></div>`)
+		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
 		Expect(ctx.RunTestScript(
 			`document.getElementById("1").insertAdjacentHTML("beforebegin", "<p>foo</p>")`,
 		)).Error().ToNot(HaveOccurred())
