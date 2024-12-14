fc01867aaadf653debe42a1db4dad735e6350104
Peter Strøiman <peter@stroiman.com>
2024-12-14 07:55:03 +0100

    Move generated file to ..._generated.go
    For now, I'd like some partial types, combine generated with
    non-generated code

--- scripting/package.go
+++ scripting/package.go
@@ -1,5 +1,5 @@
 //go:generate ../internal/code-gen/code-gen -g html-elements -o html_elements.go
-//go:generate ../internal/code-gen/code-gen -g xhr -o xml_http_request.go
+//go:generate ../internal/code-gen/code-gen -g xhr -o xml_http_request_generated.go
 
 // The scripting package implements ECMAScript execution through the v8 engine.
 package scripting
