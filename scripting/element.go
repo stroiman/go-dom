daf337bee9a8b28166f7bb5a513c830cc9f14332
Peter Strøiman <peter@stroiman.com>
2024-12-20 23:16:39 +0100

    Move argumentHelper

--- scripting/element.go
+++ scripting/element.go
@@ -46,68 +46,6 @@ func CreateElement(host *ScriptHost) *v8.FunctionTemplate {
 	return builder.constructor
 }
 
-type argumentHelper struct {
-	*v8.FunctionCallbackInfo
-	ctx *ScriptContext
-}
-
-func newArgumentHelper(host *ScriptHost, info *v8.FunctionCallbackInfo) argumentHelper {
-	ctx := host.MustGetContext(info.Context())
-	return argumentHelper{info, ctx}
-}
-
-func (h argumentHelper) GetFunctionArg(index int) (*v8.Function, error) {
-	args := h.Args()
-	if index >= len(args) {
-		return nil, ErrWrongNoOfArguments
-	}
-	arg := args[index]
-	if arg.IsFunction() {
-		return arg.AsFunction()
-	}
-	return nil, ErrIncompatibleType
-}
-
-func (h argumentHelper) GetInt32Arg(index int) (int32, error) {
-	args := h.Args()
-	if index >= len(args) {
-		return 0, ErrWrongNoOfArguments
-	}
-	arg := args[index]
-	if arg.IsNumber() {
-		return arg.Int32(), nil
-	}
-	return 0, ErrIncompatibleType
-}
-
-func (h argumentHelper) GetStringArg(index int) (string, error) {
-	args := h.Args()
-	if index >= len(args) {
-		return "", ErrWrongNoOfArguments
-	}
-	arg := args[index]
-	if arg.IsString() {
-		return arg.String(), nil
-	}
-	return "", ErrIncompatibleType
-}
-
-func (h argumentHelper) GetNodeArg(index int) (Node, error) {
-	args := h.Args()
-	if index >= len(args) {
-		return nil, ErrWrongNoOfArguments
-	}
-	arg := args[index]
-	if arg.IsObject() {
-		o := arg.Object()
-		cached, ok_1 := h.ctx.GetCachedNode(o)
-		if node, ok_2 := cached.(Node); ok_1 && ok_2 {
-			return node, nil
-		}
-	}
-	return nil, v8.NewTypeError(h.ctx.host.iso, "Must be a node")
-}
-
 var (
 	ErrIncompatibleType   = errors.New("Incompatible type")
 	ErrWrongNoOfArguments = errors.New("Not enough arguments passed")
