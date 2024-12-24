598255c677ed57f46db67f98f8db4af522cfeba2
Peter Strøiman <peter@stroiman.com>
2024-12-24 09:38:02 +0100

    Remove console log

--- scripting/url_test.go
+++ scripting/url_test.go
@@ -10,9 +10,7 @@ var _ = Describe("V8 URL", func() {
 	It("Is retrievable after construction", func() {
 		ctx := NewTestContext()
 		Expect(ctx.RunTestScript(`
-			console.log("BEFORE")
 			const u = new URL("foo/bar", "http://example.com");
-			console.log("AFTER")
 			u.href
 		`)).To(Equal("http://example.com/foo/bar"))
 	})
