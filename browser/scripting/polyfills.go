36ec98855a8b23f553e52f71883eb2e9841103e8
Peter Strøiman <peter@stroiman.com>
2024-12-30 18:06:51 +0100

    Extract xpath implementation to polyfills

--- browser/scripting/polyfills.go
+++ browser/scripting/polyfills.go
@@ -2,12 +2,27 @@ package scripting
 
 import (
 	_ "embed"
+	"errors"
 )
 
+//go:embed polyfills/xpath.js
+var xpath []byte
+
 func installPolyfills(context *ScriptContext) error {
-	return context.Run(`
+	errs := []error{
+		context.Run(`
 		FormData.prototype.forEach = function(cb) {
 			return Array.from(this).forEach(cb)
 		}
-	`)
+	`),
+		context.Run(string(xpath)),
+		context.Run(`
+			const { XPathExpression, XPathResult } = window;
+			const evaluate = XPathExpression.prototype.evaluate;
+			XPathExpression.prototype.evaluate = function (context, type, res) {
+				return evaluate.call(this, context, type ?? XPathResult.ANY_TYPE, res);
+			};
+	`),
+	}
+	return errors.Join(errs...)
 }
