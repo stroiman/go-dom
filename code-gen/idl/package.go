0d38a22c17ef28bc4565342c446bd80d250cae73
Peter Strøiman <peter@stroiman.com>
2025-01-09 20:09:14 +0100

    Add documentation about idl package

---
+++ code-gen/idl/package.go
@@ -0,0 +1,4 @@
+// Package idl contains data types representing the information present in the
+// webspec IDL files, including functions that can help cross-references
+// information.
+package idl
