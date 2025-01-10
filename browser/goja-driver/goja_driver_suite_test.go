8087480927a40141144b96d01ac578e7d0d401d1
Peter Strøiman <peter@stroiman.com>
2025-01-10 11:40:28 +0100

    Run generic script suite test for goja driver

--- browser/goja-driver/goja_driver_suite_test.go
+++ browser/goja-driver/goja_driver_suite_test.go
@@ -5,7 +5,15 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/stroiman/go-dom/browser/goja-driver"
+	. "github.com/stroiman/go-dom/browser/internal/test/script-test-suite"
 )
+
+var testSuite = NewScriptTestSuite(NewGojaScriptEngine(), "goja")
+
+func init() {
+	testSuite.CreateAllGinkgoTests()
+}
 
 func TestGojaDriver(t *testing.T) {
 	RegisterFailHandler(Fail)
