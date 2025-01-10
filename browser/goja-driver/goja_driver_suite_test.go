fa94dca43974f5bf081e3253da7f9b92bfb05a72
Peter Strøiman <peter@stroiman.com>
2025-01-10 17:04:01 +0100

    Provide custom formatting of goja exceptions

--- browser/goja-driver/goja_driver_suite_test.go
+++ browser/goja-driver/goja_driver_suite_test.go
@@ -4,8 +4,10 @@ import (
 	"log/slog"
 	"testing"
 
+	"github.com/dop251/goja"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
 	. "github.com/stroiman/go-dom/browser/goja-driver"
 	"github.com/stroiman/go-dom/browser/internal/test"
 	. "github.com/stroiman/go-dom/browser/internal/test/script-test-suite"
@@ -13,11 +15,21 @@ import (
 
 var testSuite = NewScriptTestSuite(NewGojaScriptEngine(), "goja")
 
+func FormatException(value any) (result string, ok bool) {
+	var exception *goja.Exception
+	if exception, ok = value.(*goja.Exception); ok {
+		result = exception.String()
+	}
+	return
+}
+
 func init() {
 	testSuite.CreateAllGinkgoTests()
 	var logLevel = test.InstallDefaultTextLogger()
 	logLevel.Set(slog.LevelInfo)
 	// logLevel.Set(slog.LevelDebug)
+
+	format.RegisterCustomFormatter(FormatException)
 }
 
 func TestGojaDriver(t *testing.T) {
