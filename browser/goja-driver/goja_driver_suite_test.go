66b10e1ec3462ad1f783f7aab66a6800dd632106
Peter Strøiman <peter@stroiman.com>
2025-01-10 16:07:46 +0100

    Install logger in goja test suite

--- browser/goja-driver/goja_driver_suite_test.go
+++ browser/goja-driver/goja_driver_suite_test.go
@@ -1,11 +1,13 @@
 package goja_driver_test
 
 import (
+	"log/slog"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/stroiman/go-dom/browser/goja-driver"
+	"github.com/stroiman/go-dom/browser/internal/test"
 	. "github.com/stroiman/go-dom/browser/internal/test/script-test-suite"
 )
 
@@ -13,6 +15,9 @@ var testSuite = NewScriptTestSuite(NewGojaScriptEngine(), "goja")
 
 func init() {
 	testSuite.CreateAllGinkgoTests()
+	var logLevel = test.InstallDefaultTextLogger()
+	logLevel.Set(slog.LevelInfo)
+	// logLevel.Set(slog.LevelDebug)
 }
 
 func TestGojaDriver(t *testing.T) {
