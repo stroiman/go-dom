c80346f8599a60295d9d2d668155bbaf3f525064
Peter Strøiman <peter@stroiman.com>
2025-01-15 17:24:20 +0100

    WIP, generate node

--- browser/goja-driver/package.go
+++ browser/goja-driver/package.go
@@ -1,5 +1,6 @@
-//go:generate ../../code-gen/code-gen -g scripting
+//go:generate ../../code-gen/code-gen -g goja
 
 // go:generate ../../code-gen/code-gen -g html-elements -o html_elements.go
+
 // The goja_driver package implements ECMAScript execution through the goja engine.
 package goja_driver
