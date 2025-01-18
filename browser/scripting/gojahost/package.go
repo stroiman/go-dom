12b59cfe5712ab2f541f5dc740be5306105cce30
Peter Strøiman <peter@stroiman.com>
2025-01-18 10:58:08 +0100

    Update package documentations

--- browser/scripting/gojahost/package.go
+++ browser/scripting/gojahost/package.go
@@ -1,7 +1,10 @@
 //go:generate ../../../code-gen/code-gen -g goja
 
-// The gojahost package provides an [html.ScriptHost] implementation that support
-// ECMAScript execution using goja.
+// The gojahost package provides functionality to execute client-scripts in
+// go-dom. The package is a pure Go implementation by using [goja].
+//
+// The package may not have full ECMAScript support. If this is a problem, the
+// [v8host] proves a v8 engine implementation, but requires CGo.
 //
 // See also: https://pkg.go.dev/github.com/dop251/goja
 package gojahost
--- browser/scripting/v8host/package.go
+++ browser/scripting/v8host/package.go
@@ -1,4 +1,13 @@
 //go:generate ../../../code-gen/code-gen -g scripting
 
-// The scripting package implements ECMAScript execution through the v8 engine.
+// The v8host packages provides functionality to execute client-side scripts in
+// go-dom. The engine uses v8, and requires cgo.
+//
+// This engine is based on tommie's v8go form, which automatically pulls the
+// latest v8 changes from the chromium repo.
+//
+// An alternate script engine that is implemented in pure go is found in the
+// gojahost package
+//
+// See also: https://github.com/tommie/v8go
 package v8host
