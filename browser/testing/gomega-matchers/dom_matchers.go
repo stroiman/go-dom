cc2b96d9a520a613a04ec73cedca79153a1faa37
Peter Strøiman <peter@stroiman.com>
2025-01-23 17:00:43 +0100

    Fix matcher error message template

--- browser/testing/gomega-matchers/dom_matchers.go
+++ browser/testing/gomega-matchers/dom_matchers.go
@@ -8,24 +8,24 @@ import (
 )
 
 func HaveAttribute(name string, expected interface{}) GomegaMatcher {
-	var (
-		found     bool
-		actual    string
-		matcher   GomegaMatcher
-		isMatcher bool
-	)
-	if matcher, isMatcher = expected.(GomegaMatcher); !isMatcher {
+	data := struct {
+		Attribute string
+		Found     bool
+		Actual    string
+		Matcher   GomegaMatcher
+		IsMatcher bool
+	}{
+		Attribute: name,
+	}
+	if data.Matcher, data.IsMatcher = expected.(GomegaMatcher); !data.IsMatcher {
 		return HaveAttribute(name, gomega.Equal(expected))
 	}
 	return gcustom.MakeMatcher(func(e dom.Element) (bool, error) {
-		if actual, found = e.GetAttribute(name); !found {
+		if data.Actual, data.Found = e.GetAttribute(name); !data.Found {
 			return false, nil
 		}
-		return matcher.Match(actual)
-	}).WithTemplate("Expected:\n{{.FormattedActual}}\n{{.To}} have have attribute '{{.Data.Attribute}}' {{if .Data.Found}}{{.Data.Matcher.FailureMessage .Data.Actual}}{{else}}, but it wasn't found{{end}}", struct {
-		Attribute string
-		Matcher   GomegaMatcher
-		Found     *bool
-		Actual    *string
-	}{name, matcher, &found, &actual})
+		return data.Matcher.Match(data.Actual)
+	}).WithTemplate(`Expected:\n{{.FormattedActual}}\n{{.To}} have have attribute '{{.Data.Attribute}}'
+{{ if .Data.Found}}{{.Data.Matcher.FailureMessage .Data.Actual -}}
+{{else}}  Attribute did not exist{{end}}`, &data)
 }
