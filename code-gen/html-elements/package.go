95cbbd8d47738b03212231d1cdbc712c1738088d
Peter Strøiman <peter@stroiman.com>
2025-01-30 15:32:43 +0100

    Remove code-gen

--- code-gen/html-elements/package.go
+++
@@ -1,6 +0,0 @@
-// Package htmlelements contain code to help generate code for implementing the
-// different subclasses of HTMLElement, in the browser/html package.
-//
-// This includes
-// - IDL attributes
-package htmlelements
--- code-gen/webref/idl/package.go
+++
@@ -1,4 +0,0 @@
-// Package idl contains data types representing the information present in the
-// webspec IDL files, including functions that can help cross-references
-// information.
-package idl
